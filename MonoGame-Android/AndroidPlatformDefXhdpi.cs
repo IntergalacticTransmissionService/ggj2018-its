8e541d5e5135facea08f8c86a13f551db4b87a26
lqdchrm <lqdchrm-code@yahoo.de>
2018-01-09 03:37:55 +0100

    added android project

---
+++ MonoGame-Android/AndroidPlatformDefXhdpi.cs
@@ -0,0 +1,13 @@
+﻿using MonoGame_Shared;
+
+namespace MonoGame_Android
+{
+    internal class AndroidPlatformDefXhdpi : PlatformDefs
+    {
+        public int Width => 800;
+
+        public int Height => 480;
+
+        public bool FullScreen => true;
+    }
+}
